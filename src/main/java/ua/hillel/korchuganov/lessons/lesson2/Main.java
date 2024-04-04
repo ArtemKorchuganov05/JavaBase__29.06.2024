337f80ffa080b6c6b7961c8c399591c560abbd1f
Korchuganov Artem <artemkorchuganov05@gmail.com>
2024-04-04 13:03:35 +0300

    commit all files

---
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson2/Main.java
@@ -0,0 +1,86 @@
+package ua.hillel.korchuganov.lessons.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        byte age;
+//
+//        age = 28;
+//
+//        System.out.println(age);
+//
+//        short shortVariable = 32500;
+//
+//        int intVariable = 2_000_000_324;
+//
+//        long longVariable = 4_545_454_245_434L;
+//
+//        System.out.println(longVariable);
+
+//        float floatVariable = 55.12345678901234567890F;
+//        double doubleVariable = 55.12345678901234567890;
+//
+//        System.out.println(floatVariable);
+//        System.out.println(doubleVariable);
+//
+//        char symbolA1 = 'Й';
+//        char symbolA2 = 65;
+//        char symbolA3 = '\u0041';
+//
+//        System.out.println(symbolA1);
+//        System.out.println(symbolA2);
+//        System.out.println(symbolA3);
+//
+//        boolean booleanVariableTrue = true;
+//        boolean booleanVariableFalse = false;
+//
+//        System.out.println(booleanVariableTrue);
+//        System.out.println(booleanVariableFalse);
+
+
+//        String str = "Hello";
+//
+//        System.out.println(str + " World" + 10 + 15 );
+
+//        int a = 10;
+//        Integer b = 10;
+//
+//        System.out.println(Byte.MIN_VALUE);
+//        System.out.println(Byte.MAX_VALUE);
+
+//        byte byteVariable = 50;
+//
+//        short shortVariable = byteVariable;
+//
+//        long longVariable = byteVariable;
+//
+//        char ch = 'A';
+//
+//        longVariable = ch;
+//
+//        System.out.println(longVariable);
+
+
+//        byte byteVariable = 50;
+//
+//        double doubleVariable = byteVariable;
+//
+//        System.out.println(doubleVariable);
+
+//        int age = 28432423;
+//
+//        byte byteAge =(byte) age;
+//
+//        System.out.println(byteAge);
+
+//        System.out.println((byte) 128);
+//        System.out.println((byte) 129);
+//        System.out.println((byte) 130);
+//        System.out.println((byte) 131);
+
+        final double PI_VALUE = 3.14159265358979323846;
+        System.out.println(PI_VALUE);
+
+
+    }
+}
