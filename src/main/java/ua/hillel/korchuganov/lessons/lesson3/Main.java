da212c9935c5b840b6917a5b2010371aa90470a8
Artem Korchuhanov <artemkorchuganov05@gmail.com>
2024-06-29 13:27:26 +0300

    Commit

--- src/main/java/ua/hillel/korchuganov/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson3/Main.java
@@ -2,6 +2,7 @@ package ua.hillel.korchuganov.lessons.lesson3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("fklsdfdfsff");
+        System.out.println("fklsdfdfsffdff");
     }
 }
+f
