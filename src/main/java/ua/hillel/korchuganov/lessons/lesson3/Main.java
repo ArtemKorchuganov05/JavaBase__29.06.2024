ba95923ae53808d7bdc3671abd7df1b7d657070a
Artem Korchuhanov <artemkorchuganov05@gmail.com>
2024-06-29 13:36:52 +0300

    add Main.java

--- src/main/java/ua/hillel/korchuganov/lessons/lesson3/Main.java
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson3/Main.java
@@ -2,6 +2,6 @@ package ua.hillel.korchuganov.lessons.lesson3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("fklsdfdfsfvdgffdffdff");
+        System.out.println("fklsdgffdfsfvdgffdffdff");
     }
 }
